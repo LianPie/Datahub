e1b8bbb00e0671de6ecf05eab0261bcbce742731
Maedeh-mahabadi <maedemahabadi2004@gmail.com>
2026-05-02 09:45:10 +0330

    creating basic dashboard page

---
+++ authentication/dashboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DataHub</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    <div class="container" style="text-align:center; margin-top:50px;">
+        <h1>Welcome !USERNAME!</h1>
+        <p>Your cloud storage is ready.</p>
+        <a href="logout.php">Logout</a>
+    </div>
+</body>
+</html>
