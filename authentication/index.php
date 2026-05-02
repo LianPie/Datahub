a91df31b82829600863519092ec0b012970c61df
Maedeh-mahabadi <maedemahabadi2004@gmail.com>
2026-05-02 09:34:16 +0330

    creating authentication base files

---
+++ authentication/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datahub</title>
+    <!--=============== CSS ===============-->
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+    <!--=============== REMIX ICONS ===============-->
+    <link href="assets/fonts/remixicon.css" rel="stylesheet">
+
+    <!-- =============== FAVICON ===============
+    <link rel="shortcut icon" href="" type="image/x-icon"> -->
+
+</head>
+<body>
+    
+</body>
+</html>
