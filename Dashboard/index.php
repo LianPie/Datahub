5b936fd010e364ae31b891673afaf8405ca199cb
Maedeh-mahabadi <maedemahabadi2004@gmail.com>
2026-05-07 01:16:45 +0330

    simple dashboard created

--- Dashboard/index.php
+++ Dashboard/index.php
@@ -1,22 +1,11 @@
 <?php
-session_start();
-if (!isset($_SESSION["user_id"])) {
-    header("Location: index.php");
-    exit();
-}
+include 'includes/header.php';
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Datahub</title>
-    <link rel="stylesheet" href="/Datahub/assets/css/LoginStyles.css">
-</head>
-<body>
-    <div class="container" style="text-align:center; margin-top:50px;">
-        <h1>Welcome <?php echo $_SESSION["username"] ?></h1>
-        <p>Your cloud storage is ready!</p>
-        <a href="logout.php">Logout</a>
-    </div>
-</body>
-</html>
+<div class="development-message">
+    <i class="ri-tools-line"></i>
+    <h2>Under Development</h2>
+    <p>This dashboard is currently being built. <br> New features will be available soon.</p>
+</div>
+
+<?php include 'includes/footer.php'; ?>
