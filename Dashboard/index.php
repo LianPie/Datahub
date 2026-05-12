31c05ae9cab58c655a361b792fcd70f966f5c214
LianPie <SonyWertky@gmail.com>
2026-05-12 15:09:52 +0330

    localize dashboard dev page

--- Dashboard/index.php
+++ Dashboard/index.php
@@ -4,8 +4,8 @@ include 'includes/header.php';
 
 <div class="development-message">
     <i class="ri-tools-line"></i>
-    <h2>Under Development</h2>
-    <p>This dashboard is currently being built. <br> New features will be available soon.</p>
+    <h2><?= __('under_development') ?></h2>
+    <p><?= __('dashboard_being_built') ?> <br> <?= __('new_features_soon') ?></p>
 </div>
 
 <?php include 'includes/footer.php'; ?>
