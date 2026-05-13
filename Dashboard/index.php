582fb1eaee6f7f7012ba0f255912b2ce4a5b9b21
Maedeh-mahabadi <maedemahabadi2004@gmail.com>
2026-05-13 10:01:11 +0330

    adding dashboard  Upload page

--- Dashboard/index.php
+++ Dashboard/index.php
@@ -26,8 +26,7 @@ include 'includes/header.php';
                             <button class="preview-btn" data-path="#" data-type="#"><i class="ri-eye-line"></i></button>
                             <a href="#" download><i class="ri-download-line"></i></a>
                             <button class="delete-btn" data-id="#"><i class="ri-delete-bin-line"></i></button>
-                                <button class="share-btn" data-id="#"><i class="ri-share-line"></i></button>
-                                <a href="#" target="_blank"><i class="ri-links-line"></i></a>
+                            <button class="share-btn" data-id="#"><i class="ri-share-line"></i></button>
                         </div>
                     </div>
         </div>
